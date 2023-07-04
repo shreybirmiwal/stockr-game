2e518a09dffaa28eb1282f2dde966a3b2c0813f2
Shrey <shreybirmiwal@gmail.com>
2023-07-04 15:40:50 -0500

    Working correct solution showing

--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -12,10 +12,19 @@ if (typeof Highcharts === "object") {
 
 const Game = () => {
   const [data, setData] = useState(null);
+  const [dragData, setDragData] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [dataHigh, setDataHigh] = useState();
+  const [dataLow, setDataLow] = useState();
+  const [lastData, setLastData] = useState();
+  const [futureData, setFutureData] = useState()
   const chartRef = useRef(null);
 
+  var tempLow = 99999999;
+  var tempHigh = -999999999;
+  var tempLast = 0;
+
   useEffect(() => {
-    fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&outputsize=full&apikey=demo')
+    fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=HOOD&outputsize=full&apikey=JLAV6W6AQIAQREW1')
       .then(response => response.json())
       .then(data => {
         const transformedData = transformData(data);
@@ -31,107 +40,209 @@ const Game = () => {
     const transformedData = [];
     let count = 0;
 
-    for (const date in timeSeriesData) {
-      if (timeSeriesData.hasOwnProperty(date)) {
-        const { '1. open': open, '2. high': high, '3. low': low, '4. close': close, '6. volume': volume } = timeSeriesData[date];
-        const formattedDate = new Date(date);
-        const dataPoint = { date: formattedDate, open: parseFloat(open), high: parseFloat(high), low: parseFloat(low), close: parseFloat(close), volume: parseInt(volume) };
-        transformedData.push(dataPoint);
+    var first = true
+    var second  = false
+    var firstDate;
+    var secondDate;
 
-        count++;
-        if (count === 50) {
-          break; // Stop processing after the first 30 data points
+    for (const date in timeSeriesData) {
+      if(first){
+        if (timeSeriesData.hasOwnProperty(date)) {
+          firstDate = new Date(date);
+          first = false
+          second = true
+        }
+      }
+      else if (second){
+        secondDate = new Date(date);
+        second = false
+      }
+      else{
+      
+        if (timeSeriesData.hasOwnProperty(date)) {
+          const { '1. open': open, '2. high': high, '3. low': low, '4. close': close, '6. volume': volume } = timeSeriesData[date];
+          const formattedDate = new Date(date);
+
+          if (parseFloat(low) < parseFloat(tempLow)) {
+            tempLow = parseFloat(low);
+          }
+          if (parseFloat(high) > parseFloat(tempHigh)) {
+            console.log("NEW HIGH " + high + " temphigh: " + tempHigh)
+            tempHigh = parseFloat(high);
+          }
+
+          const dataPoint = { date: formattedDate, open: parseFloat(open), high: parseFloat(high), low: parseFloat(low), close: parseFloat(close), volume: parseFloat(volume) };
+          transformedData.push(dataPoint);
+
+          count++;
+
+          if (count === 30) {
+            tempLast = close
+          }
+
+          if (count === 50) {
+            break; // Stop processing after the first 30 data points
+          }
         }
       }
     }
+
+    //add highs and lows
+    const highPoint = { date: firstDate, open: tempHigh, high: tempHigh, low: tempHigh, close: tempHigh, volume: 0 };
+    const lowPoint = { date: secondDate, open: tempLow, high: tempLow, low: tempLow, close: tempLow, volume: 0 };
+    transformedData.push(highPoint);
+    transformedData.push(lowPoint);
+
+    setLastData(parseFloat(tempLast))
+    setDragData([parseFloat(tempLast),parseFloat(tempLast),parseFloat(tempLast),parseFloat(tempLast),parseFloat(tempLast),parseFloat(tempLast),parseFloat(tempLast),parseFloat(tempLast),parseFloat(tempLast), parseFloat(tempLast)])
+    setDataLow(tempLow)
+    setDataHigh(tempHigh)
+
     transformedData.reverse();
-    const future20 = transformedData.splice(-20);
-    console.log(transformedData);
-    console.log(future20);
+    var future20 = transformedData.splice(-20);
+    future20 = future20.map(item => item.close);
+    setFutureData(future20)
+
+    console.log("DATA : " + transformedData)
+    console.log("FUTURE : " + future20)
     return transformedData;
   };
+
 
   if (data === null) {
     return <div>Loading...</div>;
   }
 
+  const handleSubmit = async () => {
+    const chart = chartRef.current.chart;
+
+    var onePercent = parseFloat((dataHigh - dataLow) / 100.0);
+    var bottom = parseFloat(dataLow);
+
+    console.log("ONE PERCENT " + onePercent)
+    console.log("BOTTOM " + bottom)
+
+    // Get the data from the draggable points
+    const pointsData = chart.series[0].points.map((point) => ({
+      x: point.x,
+      y: point.y,
+      z: (point.y*onePercent)+bottom
+    }));
+
+    //update the solution graph
+    var percentages = futureData
+    for (var i = 0; i < percentages.length; i++) {
+      percentages[i] = (percentages[i] - bottom) / onePercent;
+    }
+    console.log("PERCENTAGES " + percentages)
+    chart.series[1].setData(percentages);
+    chart.series[1].update({ visible: true });
+
+
+    console.log(pointsData);
+  };
+    
+
   return (
     <div>
-
-    <div className="flex flex-row">
-      <div className="w-1/2 h-96">
-        <Chart data={data} />
-      </div>
-      <div className="w-1/2 h-96">
-        <HighchartsReact
-          highcharts={Highcharts}
-          ref={chartRef}
-          constructorType={"chart"}
-          options={{
-            chart: {
-              type: 'line',
-              backgroundColor: '#ffffcc',
-            },
-            xAxis: {
-              title: {
-                text: null,
+      <div className="flex flex-row m-8">
+        <div className="w-7/12 h-96">
+          <Chart data={data} />
+        </div>
+        <div className="w-5/12 h-max">
+          <HighchartsReact
+            highcharts={Highcharts}
+            ref={chartRef}
+            constructorType={"chart"}
+            options={{
+              chart: {
+                type: 'line',
+                backgroundColor: '#ffffcc',
               },
-              visible: false,
-            },
-            yAxis: {
-              title: {
-                text: null,
-              },
-              visible: false,
-              min: 0,
-              max: 100,
-            },
-            series: [
-              {
-                name: '',
-                data: [10, 10, 9, 8, 7, 20, 23, 42],
-                lineWidth: 2,
-                dragDrop: {
-                  draggableX: false,
-                  draggableY: true,
-                  dragMinY: 10, // minimum y-value allowed for dragged points
-                  dragMaxY: 90, // maximum y-value allowed for dragged points
-                  dragPrecisionY: 1, // number of decimal places for y-values
+              xAxis: {
+                title: {
+                  text: null,
                 },
-                point: {
-                  events: {
-                    // drop: handlePointDrag
-                  },
-                },
-              },
-              {
-                name: 'Solution',
-                data: [10, 10, 10, 10],
-                lineWidth: 2,
                 visible: false,
               },
-            ],
-            title: {
-              text: null,
-            },
-            credits: {
-              enabled: false,
-            },
-            legend: {
-              enabled: false,
-            },
-            plotOptions: {
-              series: {},
-            },
-          }}
-        />
+              yAxis: {
+                title: {
+                  text: null,
+                },
+                visible: false,
+                min: 0,
+                max: 100,
+              },
+              series: [
+                {
+                  name: '',
+                  data: [50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50,50],
+                  lineWidth: 2,
+                  dragDrop: {
+                    draggableX: false,
+                    draggableY: true,
+                    dragMinY: 0, // minimum y-value allowed for dragged points
+                    dragMaxY: 100, // maximum y-value allowed for dragged points
+                    dragPrecisionY: 1, // number of decimal places for y-values
+                  },
+                  point: {
+                    events: {
+                      // drop: handlePointDrag
+                    },
+                  },
+                },
+                {
+                  name: 'Solution',
+                  data: [10, 10, 10, 10],
+                  lineWidth: 2,
+                  visible: false,
+                },
+              ],
+              dataLabels: [
+                {
+                  enabled: true,
+                  format: 'Min: {point.y}',
+                  align: 'left',
+                  x: 5,
+                  y: -5,
+                },
+                {
+                  enabled: true,
+                  format: 'Max: {point.y}',
+                  align: 'left',
+                  x: 5,
+                  y: 15,
+                },
+              ],
+              title: {
+                text: null,
+              },
+              credits: {
+                enabled: false,
+              },
+              legend: {
+                enabled: false,
+              },
+              plotOptions: {
+                series: {},
+              },
+            }}
+          />
+        </div>
       </div>
-    </div>
-          <div className='mt-60 ml-10'>
-            <div className="flex flex-row w-56 items-center justify-center bg-gray-200 hover:bg-gray-400 rounded-md shadow-md cursor-pointer">
-              <h1 className='p-5 text-2xl font-bold flex text-center align-middle justify-center'> Parse Memo </h1>
-            </div>
-          </div>
+      
+      <div className='mt-60 ml-14' >
+        <div  onClick={handleSubmit} className="flex flex-row w-56 items-center justify-center bg-gray-200 hover:bg-gray-400 rounded-md shadow-md cursor-pointer">
+        <h1 className='p-5 text-2xl font-bold flex text-center align-middle justify-center'> Submit </h1>
+        </div>
+
+        {/*
+          <h1 className='p-5 text-2xl font-bold flex '> low {dataLow} </h1>
+          <h1 className='p-5 text-2xl font-bold flex '> high {dataHigh} </h1>
+          <h1 className='p-5 text-2xl font-bold flex '> last close {lastData} </h1>
+         */}
+
+      </div>
     </div>
   );
 };
