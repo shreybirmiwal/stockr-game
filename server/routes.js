6acd3f6e5ecd8d9af3532e2b014e28be0601e6ab
Shrey <shreybirmiwal@gmail.com>
2023-07-12 10:11:03 -0500

    Get trading days api call

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router()
-
+var smc = require('stock-market-clock');
 
 router.get('/first', (req, res) => {
     res.json({ message: "first"});
 });
+
+
+//if it is a trading day, show current chart
+//return [true, nexttradingday]
+
+//if it is not a trading day, show the next chart
+// return [false, nexttradingday]
+
+router.get('/trading_day', (req, res) => {
+
+    marketTimeData(isoString, skipFindNext=false)
+    console.log(smc.marketTimeData("2022-09-23T19:59:00.000Z"))
+    res.json(smc.marketTimeData("2022-09-23T19:59:00.000Z"))
+})
 
 module.exports = router;
